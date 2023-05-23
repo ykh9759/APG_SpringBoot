f8d19152e2f0f209299f94a0dcf211d673592b1a
unknown <ykh059@naver.com>
2023-05-23 22:41:08 +0900

    20230525 첫 커밋

---
+++ src/main/java/com/example/apg/ApgApplication.java
@@ -0,0 +1,13 @@
+package com.example.apg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apg/ApgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
