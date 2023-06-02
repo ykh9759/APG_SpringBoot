4557196abb579f4b4ae14736991b57282ad29859
unknown <ykh059@naver.com>
2023-06-02 23:52:27 +0900

    20230602 vue router 추가 및 view페이지 추가

---
+++ Vue/src/views/Home.vue
@@ -0,0 +1,13 @@
+
+<template>
+    <header>
+        <h1>AuctionPlayGround</h1>
+    </header>
+    <main>
+        <div class="row">
+            <div class="col-4">
+                <button class="btn btn-primary">만들기</button>
+            </div>
+        </div>
+    </main>
+</template>
