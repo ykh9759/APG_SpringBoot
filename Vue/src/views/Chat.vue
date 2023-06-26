80106eb4c6e301d2391ddf807011a82823577c83
unknown <ykh059@naver.com>
2023-06-26 22:05:07 +0900

    20230626 vue 소켓 추가 및 spring 개발

--- Vue/src/views/Chat.vue
+++ Vue/src/views/Chat.vue
@@ -6,7 +6,7 @@
       </span>
     </div>
     <chat-list :msgs="msgData"></chat-list>
-    <chat-form @submitMessage="sendMessage"></chat-form>
+    <chat-form @submitMessage="send"></chat-form>
   </div>
 </template>
 
@@ -15,6 +15,8 @@ import { mapMutations, mapState } from "vuex";
 import ChatList from "@/components/ChatList.vue";
 import ChatForm from "@/components/ChatForm.vue";
 import Constant from "@/Constant";
+import Stomp from 'webstomp-client'
+import SockJS from 'sockjs-client'
 
 export default {
   data() {
@@ -34,18 +36,19 @@ export default {
   created() {
     const storedData = localStorage.getItem('user');
     this.userData = JSON.parse(storedData);
+    this.connect();
   },
 
-  mounted() {
-    this.$socket.on("chat", (data) => {
-      this.pushMsgData(data);
+  // mounted() {
+  //   this.$socket.on("chat", (data) => {
+  //     this.pushMsgData(data);
 
-      setTimeout(() => {
-        const element = document.getElementById("chat__body");
-        element.scrollTop = element.scrollHeight;
-      }, 0);
-    });
-  },
+  //     setTimeout(() => {
+  //       const element = document.getElementById("chat__body");
+  //       element.scrollTop = element.scrollHeight;
+  //     }, 0);
+  //   });
+  // },
 
   methods: {
     ...mapMutations({
@@ -75,6 +78,46 @@ export default {
         element.scrollTop = element.scrollHeight;
       }, 0);
     },
+    send(msg) {
+
+      console.log("Send message:" + msg);
+      if (this.stompClient && this.stompClient.connected) {
+
+        const data = { 
+          userName: this.userData.userName,
+          content: msg 
+        };
+
+        this.stompClient.send("/receive", JSON.stringify(data), {});
+      }
+    }, 
+    connect() {
+      const serverURL = "http://localhost:8080"
+      let socket = new SockJS(serverURL);
+      this.stompClient = Stomp.over(socket);
+      console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
+      this.stompClient.connect(
+        {},
+        frame => {
+          // 소켓 연결 성공
+          this.connected = true;
+          console.log('소켓 연결 성공', frame);
+          // 서버의 메시지 전송 endpoint를 구독합니다.
+          // 이런형태를 pub sub 구조라고 합니다.
+          this.stompClient.subscribe("/send", res => {
+            console.log('구독으로 받은 메시지 입니다.', res.body);
+
+            // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
+            this.recvList.push(JSON.parse(res.body))
+          });
+        },
+        error => {
+          // 소켓 연결 실패
+          console.log('소켓 연결 실패', error);
+          this.connected = false;
+        }
+      );        
+    }
   },
 };
 </script>
