c644e2c17c009347338a6d68e9c77c4e7ce42521
unknown <ykh059@naver.com>
2023-07-02 21:10:48 +0900

    20230702 docker수정 및 spring수정

--- spring/src/main/java/com/example/cpg/controller/FrontController.java
+++ spring/src/main/java/com/example/cpg/controller/FrontController.java
@@ -9,22 +9,21 @@ import com.example.cpg.domain.ChatVo;
 @Controller
 public class FrontController {
 
-    // /receive를 메시지를 받을 endpoint로 설정합니다.
+    // /receive를 메시지를 받을 endpoint로 설정
     @MessageMapping("/receive")
-    
     // /send로 메시지를 반환합니다.
     @SendTo("/send")
-    
-    // SocketHandler는 1) /receive에서 메시지를 받고, /send로 메시지를 보내줍니다.
-    // 정의한 SocketVO를 1) 인자값, 2) 반환값으로 사용합니다.
+    // SocketHandler는 1) /receive에서 메시지를 받고, /send로 메시지를 보내준다
     public ChatVo SocketHandler(ChatVo socketVO) {
-        // vo에서 getter로 userName을 가져옵니다.
+
         String userName = socketVO.getUserName();
-        // vo에서 setter로 content를 가져옵니다.
         String content = socketVO.getContent();
 
         // 생성자로 반환값을 생성합니다.
-        ChatVo result = new ChatVo(userName, content);
+        ChatVo result = ChatVo.builder()
+                            .userName(userName)
+                            .content(content)
+                            .build();
         // 반환
         return result;
     }
--- spring/src/main/java/com/example/cpg/domain/ChatVo.java
+++ spring/src/main/java/com/example/cpg/domain/ChatVo.java
@@ -1,15 +1,20 @@
 package com.example.cpg.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Builder;
 
 @Data
-@AllArgsConstructor
 public class ChatVo {
     // 유저의 이름을 저장하기 위한 변수
     private String userName;
 
     // 메시지의 내용을 저장하기 위한 변수
     private String content;
+
+    @Builder
+    public ChatVo(String userName, String content ) {
+        this.userName = userName;
+        this.content = content;
+    }
     
 }
